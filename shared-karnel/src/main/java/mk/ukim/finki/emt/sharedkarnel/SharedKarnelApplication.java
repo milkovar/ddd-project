680721145decaadb983053a65e5608f50a9455b4
milkovar <rosana.milkova@gmail.com>
2021-09-22 18:37:12 +0200

    Initial commit

---
+++ shared-karnel/src/main/java/mk/ukim/finki/emt/sharedkarnel/SharedKarnelApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.sharedkarnel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedKarnelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SharedKarnelApplication.class, args);
+    }
+
+}
---
+++ shared-karnel/src/test/java/mk/ukim/finki/emt/sharedkarnel/SharedKarnelApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.emt.sharedkarnel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedKarnelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
